f5d283dfd3252f5ab1c4a45b86fd85bf810d0018
Morgan Trotter <mhtrotte@ucsc.edu>
2021-01-02 23:39:36 -0800

    Updates

---
+++ precache-manifest.7c5241659b380693f30f610f677c931f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a7270f335281d3ce46b033019db28fc",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "1d44e3eb16d7e9fdb5b6",
+    "url": "/react-portfolio/static/css/2.43079d8c.chunk.css"
+  },
+  {
+    "revision": "a0352b2a4d1f5af12c21",
+    "url": "/react-portfolio/static/css/main.c8bd914e.chunk.css"
+  },
+  {
+    "revision": "1d44e3eb16d7e9fdb5b6",
+    "url": "/react-portfolio/static/js/2.041eb2fe.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.041eb2fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0352b2a4d1f5af12c21",
+    "url": "/react-portfolio/static/js/main.1d72a686.chunk.js"
+  },
+  {
+    "revision": "73a871a578ef5a2aeb49",
+    "url": "/react-portfolio/static/js/runtime-main.1d992053.js"
+  }
+]);
